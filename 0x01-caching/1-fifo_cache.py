1de19ea37d30af62aaf16b42b795563b95cd97fa
Scion-Kin <dinturner17@gmail.com>
2024-06-25 17:26:08 +0200

    Task 1 done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
-''' This module defines a function '''
+''' This module defines a class '''
+
+from base_caching import BaseCaching as Base
+
+
+class FIFOCache(Base):
+    ''' Improves the base class '''
+
+    def __init__(self):
+        ''' Initializes the instance '''
+
+        super().__init__()
+
+    def put(self, key, item):
+        ''' This insert an item into the cache '''
+
+        if key is None or item is None:
+            pass
+
+        else:
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > self.MAX_ITEMS:
+                for i in self.cache_data:
+                    print("DISCARD: {}".format(i))
+                    del self.cache_data[i]
+                    break
+
+    def get(self, key):
+        ''' This retrieves an item from the cache '''
+
+        return self.cache_data.get(key, None)
