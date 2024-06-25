75cc37db0a8d49169a80a23d75e667915a8f0a58
Scion-Kin <dinturner17@gmail.com>
2024-06-25 19:31:27 +0200

    Task 3 done

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
-''' This module defines a function '''
+''' This module defines a class '''
+
+from base_caching import BaseCaching as Base
+
+
+class LRUCache(Base):
+    ''' Improves the base class '''
+
+    def put(self, key, item):
+        ''' This insert an item into the cache and
+            moves the item to the front of the queue '''
+
+        if key is not None and item is not None:
+            li = [(i, j) for i, j in self.cache_data.items() if i != key]
+            li = li + [(key, item)]
+
+            if len(li) > self.MAX_ITEMS:
+                print("DISCARD: {}".format(li[0][0]))
+                li.pop(0)
+
+            self.cache_data = dict(li)
+
+    def get(self, key):
+        ''' This retrieves an item from the cache and
+            moves the accessed item to the front of the queue '''
+
+        li = [(i, j) for i, j in self.cache_data.items() if i != key]
+        if key in self.cache_data:
+            self.cache_data = dict(li + [(key, self.cache_data[key])])
+
+        return self.cache_data.get(key, None)
