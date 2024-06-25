16ce68d1aef9b53979d2837703fdb7308b4370a3
Scion-Kin <dinturner17@gmail.com>
2024-06-25 18:12:47 +0200

    Corrected mistake in task 2's solution

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,13 +15,17 @@ class LIFOCache(Base):
     def put(self, key, item):
         ''' This insert an item into the cache '''
 
-        if key is None or item is None:
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
+
             self.cache_data[key] = item
 
             if len(self.cache_data) > self.MAX_ITEMS:
                 last_in = [i for i in self.cache_data.items()][-2][0]
                 print("DISCARD: {}".format(last_in))
                 del self.cache_data[last_in]
+                
 
     def get(self, key):
         ''' This retrieves an item from the cache '''
