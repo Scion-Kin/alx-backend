db15aed66b5a647987da7104f7088caf060dad93
Scion-Kin <dinturner17@gmail.com>
2024-07-02 17:39:59 +0200

    Task 0 done

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+''' This contains a flask application '''
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    ''' A configuration class '''
+
+    LANGUAGES = ['en', 'fr']
+
+
+app = Flask(__name__)
+babel = Babel(app, Config.LANGUAGES)
+
+
+@app.route('/')
+def home():
+    ''' This is the index route '''
+
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+
+    app.run()
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions on the i18n project
