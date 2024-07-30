1db643314defec49a3fcae89eb704355e1c5ca5f
Scion-Kin <dinturner17@gmail.com>
2024-07-30 19:42:20 +0200

    Multiple tasks done

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,15 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const job = queue.create('push_notification_code', {
+                  phoneNumber: '4153518780',
+                  message: 'This is the code to verify your account'
+                });
+job.save(error => {
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job.on('complete', result => console.log('Notification job completed'));
+job.on('failed', result => console.log('Notification job completed'));
