6263a15d09e076ad23a79ff27e09d7f72b58278e
Scion-Kin <dinturner17@gmail.com>
2024-07-31 11:36:06 +0200

    Task 8 and 9 done

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -4,21 +4,22 @@ const queue = kue.createQueue();
 const blacklist = ['4153518780', '4153518781'];
 
 function sendNotification(phoneNumber, message, job, done) {
-  let total = 2, pending = 2;
+  let total = 2, pending = 3;
   while (pending !== 0) {
+    pending--;
     if (total - pending <= total / 2) {
       job.progress(total - pending, total);
     }
-    if (blacklist.includes(job.data.phoneNumber)) {
-        done(`Phone number ${phoneNumber} is blacklisted`);
+    if (blacklist.includes(phoneNumber)) {
+      done(`Phone number ${phoneNumber} is blacklisted`);
+      break;
     }
     else {
       if (total == pending) {
-        console.log(`Sending notification to ${job.data.phoneNumber}, with message: ${message}`);
+        console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
       }
       done();
     }
-    pending--;
   }
 }
 
